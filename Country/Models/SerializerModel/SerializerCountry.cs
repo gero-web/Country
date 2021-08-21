8651b2b01d3b8f467c538099ccdc87487e3cc047
knlih <psa-98@mail.ru>
2021-08-21 19:46:44 +0800

    Валидация

--- Country/Models/SerializerModel/SerializerCountry.cs
+++ Country/Models/SerializerModel/SerializerCountry.cs
@@ -1,25 +1,31 @@
 ﻿using System.ComponentModel;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace Country.Models.SerializerModel
 {
     public class SerializerCountry
     {
+        [Required(ErrorMessage = "Поле не должно быть пустым!")]
         [DisplayName("Страна")]
         public string name { get; set; }
 
+        [Required(ErrorMessage = "Поле не должно быть пустым!")]
         [DisplayName("Столица")]
         public string capital { get; set; }
 
+        [Required(ErrorMessage = "Поле не должно быть пустым!")]
         [DisplayName("Население")]
         public int? population { get; set; }
 
+        [Required(ErrorMessage = "Поле не должно быть пустым!")]
         [DisplayName("Площадь")]
         public double? area { get; set; }
 
+        [Required(ErrorMessage = "Поле не должно быть пустым!")]
         [DisplayName("Регион")]
         public string region { get; set; }
 
+        [Required(ErrorMessage = "Поле не должно быть пустым!")]
         [DisplayName("Код страны")]
         public string numericCode { get; set; }
 
